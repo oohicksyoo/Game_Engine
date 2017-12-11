19c793e6b26497b01857511fbdccbe4ccce526ff
alex hicks <alexhicks@live.ca>
2017-12-10 19:16:42 -0500

    Lets combine it all
    - Combine SFML & Dear IMGUI sample with LUA integration
    - Get everything working at same time

--- GameEngine/GameEngine/Scripts/script.lua
+++ GameEngine/GameEngine/Scripts/script.lua
@@ -1,2 +1,9 @@
-testString = "This is the cats meow"
+--[[testString = "This is the cats meow"
 number = 69
+
+Utility.printMessage("Go fuck yourself!")--]]
+
+sumNumbers = function(a, b)
+    Utility.printMessage("Hello C++");
+    return a + b
+end
